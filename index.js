da1cec85b9cf93f2b4a94f42ef1e9b9a6b479314
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-03-30 20:28:14 +0200

    :sparkles: Send notification instead of log

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const notifier = require('node-notifier');
 
 (async () => {
   let mixnodeCount = -1;
@@ -16,7 +17,10 @@ const fetch = require('node-fetch');
       console.log(`Mixnode count: ${newMixnodeCount}, previous count: ${mixnodeCount}`);
 
       if (mixnodeCount > newMixnodeCount) {
-        console.log('SPIN UP THE FCK UR NODE!!!');
+        notifier.notify({
+          title: 'Nym Mixnodes',
+          message: `Noticed a drop in active nym mixnodes: ${mixnodeCount}`
+        });
       }
 
     }
